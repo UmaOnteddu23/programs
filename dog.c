dcef1b4c37e2f272cf2f6aa230782c8734b724d9
Uma Onteddu <umaonteddu23@gmail.com>
2026-01-30 15:55:57 +0530

    Add files via upload

---
+++ dog.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int age,result;
+    printf("Enter age of dog: ");
+    scanf("%d",&age);
+    result=age*7;
+    printf("The age of dog in human years is:%d",result);
+    return 0;
+}
